3dff9616dca1a8eecb09e32a5b2d20c7168e38b8
Peri Moritz <peri.moritz@gmail.com>
2018-05-16 18:35:31 +0100

    Added Slick Carousel and updated calendar .. boo haw!

--- public/assets/js/javascript.js
+++ public/assets/js/javascript.js
@@ -2,3 +2,14 @@ $('.navbar-toggler').on('click','.fas', function(){
   console.log('hi')
   $(this).toggleClass('fa-bars fa-times');
 } );
+
+//   Slick Slider
+$(document).ready(function() {
+
+  $('.single-item').slick({
+   mobileFirst: true,
+   fade: true,
+  });
+  
+  })
+  
