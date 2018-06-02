c2840fed97435d64357dbb5d576868a0d5454d35
Peri Moritz <peri.moritz@gmail.com>
2018-06-02 20:18:24 +0100

    Clean up css, restructure sass folders

--- public/assets/js/javascript.js
+++ public/assets/js/javascript.js
@@ -23,3 +23,12 @@ $('.navbar-nav > li > a').on('click', function(){
 if (window.location.hash) {
     smoothScroll(window.location.hash)
 }
+
+// Hamburger Animation Credit: http://w3bits.com/animated-hamburger-icons/
+var el = document.querySelectorAll('.hamburger');
+	for(i=0; i<=el.length; i++) {
+		el[i].addEventListener('click', function() {
+			this.classList.toggle('active');
+		}, false);
+	}
+
