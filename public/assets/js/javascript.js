f4c64ed67f3fccf14e31c74c63f45d0d4bdb0c4d
Peri Moritz <peri.moritz@gmail.com>
2018-06-14 21:15:47 +0100

    Parallax javascript fixes

--- public/assets/js/javascript.js
+++ public/assets/js/javascript.js
@@ -23,17 +23,19 @@ $('.navbar-nav > li > a').on('click', function(){
 if (window.location.hash) {
     smoothScroll(window.location.hash)
 }
+// Jarallax 
+// object-fit polyfill run
+objectFitImages();
+
+/* init Jarallax */
+jarallax(document.querySelectorAll('.jarallax'));
+
 
 // Hamburger Animation Credit: http://w3bits.com/animated-hamburger-icons/
+
 var el = document.querySelectorAll('.hamburger');
-	for(i=0; i<=el.length; i++) {
+	for(i=0; i<el.length; i++) {
 		el[i].addEventListener('click', function() {
 			this.classList.toggle('active');
 		}, false);
     }
-    
-
-
-
-
-
