d266cfa0234946590cd3c74874904551e775d17f
Wolfgang Moritz <wolfmoritz@gmail.com>
2018-07-09 20:17:30 +0100

    Reordered code, and added hamburger fix for in page links.

--- public/assets/js/javascript.js
+++ public/assets/js/javascript.js
@@ -1,39 +1,34 @@
-$('.navbar-toggler').on('click','.fas', function(){
-  $(this).toggleClass('fa-bars fa-times');
-} );
-
 //   Slick Slider
 $('.single-item').slick({
     mobileFirst: true,
     fade: true,
 });
 
+// Hamburger Animation Credit: http://w3bits.com/animated-hamburger-icons/
+var el = document.querySelectorAll('.hamburger');
+    for(i=0; i<el.length; i++) {
+        el[i].addEventListener('click', function() {
+            this.classList.toggle('active');
+        }, false);
+    }
+
+// Scroll with offset to named anchor
 var smoothScroll = function(hash) {
     $('html, body').animate({
         scrollTop: $(hash).offset().top - 100
     }, 500, 'easeInOutSine');
 }
-
-// Close BS4 navbar on in-page links, and scroll with offset
+// Close BS4 navbar on in-page links
 $('.navbar-nav > li > a').on('click', function(){
     $('.navbar-collapse').collapse('hide');
+    $('.hamburger').toggleClass('active collapsed');
     smoothScroll($(this).prop('hash'))
 });
 
+// If deep linking to a named anchor, scroll to target
 if (window.location.hash) {
     smoothScroll(window.location.hash)
 }
 // Jarallax
-// object-fit polyfill run
 objectFitImages();
-
-/* init Jarallax */
 jarallax(document.querySelectorAll('.jarallax'));
-
-// Hamburger Animation Credit: http://w3bits.com/animated-hamburger-icons/
-var el = document.querySelectorAll('.hamburger');
-	for(i=0; i<el.length; i++) {
-		el[i].addEventListener('click', function() {
-			this.classList.toggle('active');
-		}, false);
-    }
