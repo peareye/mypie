a3103af4c9c334c1da4fa8891342b6f8ae2966af
Wolfgang Moritz <wolfmoritz@gmail.com>
2019-12-26 18:50:47 +0000

    Updated docker images.

--- docker/webserver/configmail.sh
+++ docker/webserver/configmail.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
+# Set container ID in hosts file
 echo "127.0.0.1 localhost localhost.localdomain $(hostname)" >> /etc/hosts
-
 yes Y | /usr/sbin/sendmailconfig
-
-service apache2 restart
-
-# To avoid closing the process, run this indefinitely
-/bin/bash
